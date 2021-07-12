ab86d6e21578593c0862289206c00b35d4eef6e2
Darkhan Akmyrza <darkhan_akmyrza@epam.com>
2021-07-12 10:20:17 +0500

    first commit

---
+++ src/main/java/com/example/jenkins1example/Jenkins1exampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins1example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins1exampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jenkins1exampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkins1example/Jenkins1exampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins1example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkins1exampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
